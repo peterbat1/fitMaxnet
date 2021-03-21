4011315c880eda446acb42459bb4dd251999fd26
Peter D Wilson <peterdonaldwilson@gmail.com>
2021-03-21 15:46:18 +1100

    Tweaks to help for bufferPoints and maskExtrapolation

--- R/bufferPoints.R
+++ R/bufferPoints.R
@@ -40,14 +40,12 @@ bufferPoints <- function(occ_pts, bufferDist_km = 200, trace = FALSE)
     stop("occ_pts must be class 'sf'")
   }
 
-
   if (trace) cat("make buffer polygon\n")
   ptsBuffer <- sf::st_union(sf::st_buffer(occ_pts_albers, dist = bufferDist))
 
   # Clip the buffer polygon to the Australian coastline
   if (trace) cat("clip buffer polygon to OZ coastline\n")
   clippedBuffer <<- st_sf(sf::st_union(sf::st_intersection(ptsBuffer, sf::st_transform(ozPolygon, 3577))))
-
 
   if (trace) cat("transform clipped buffer to CRS of occ_pts\n")
   if (pts_crs != 3577) clippedBuffer <- sf::st_transform(clippedBuffer, crs = pts_crs)
