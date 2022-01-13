0fcf1511fd74da4e44a8e34b621989339bf59461
Peter D Wilson <peterdonaldwilson@gmail.com>
2022-01-13 22:21:46 +1100

    Completed dev of computeMESS()

--- R/computeMESS.R
+++ R/computeMESS.R
@@ -1,22 +1,18 @@
-
-
-
-
-
-
-
 #' Compute MESS rasters
 #'
 #' Compute set of MESS rasters for a maxnet model
 #'
 #' @param thisModel A maxnet model object
-#' @param envData raster stack. The environmental predictor raster layers used to fit the maxnet model
+#' @param envStack raster stack. The environmental predictor raster layers used to fit the maxnet model
+#' @param occSWD numeric matrix. Reference data; typically created by concatenating occurrence SWD and background SWD files used to fit the model
+#' @param bkgSWD numeric matrix
 #' @param outPath Character. Path to folder into which output raster layers will be written
+#' @param varImpThreshold Numeric. Value used to filter variables by their importance scores; percentage importance therefore ranges from 0 to 100.
 #' @param MESSonly Logical. Compute only the MESS raster layer? Default is TRUE. If FALSE, then MESS component rasters for each variable plus the MESS raster layer are computed
 #' @param ... Optional parameters passed to the raster function writeRaster
 #' @details
 #'
-#' A MESS computation is performed using the function mess in the package dismo. This function is a wrapper which orchestrates a call to the mess function using information stored in the maxnet model object.
+#' A MESS computation is performed blah blah.
 #'
 #' To facilitate the processing of very large, high-resolution rasters, only those variables which are used in the final model are included in the computation of MESS output.
 #'
@@ -27,63 +23,99 @@
 #' @examples
 #' \dontrun{}
 #'
-computeMESS <- function(thisModel = NULL, envData = "", outPath = "", MESSonly = TRUE, ...)
+computeMESS <- function(thisModel = NULL, envPath = NULL, occSWD, bkgSWD, varImpThresold = 0, outPath = "", MESSonly = TRUE, ...)
 {
   if (!("maxnet" %in% class(thisModel)))
     stop("'thisModel' is not a maxnet model object")
 
-  # if (!dir.exists(envDataPath))
-  #   stop("Cannot find folder given in 'envDataPath'")
-  #
-  # if (!file.exists(swdFilename))
-  #   stop("Cannot find SWD file referenced in 'swdFilename'")
+  envFileSet <- list.files(envPath, "*.tif", full.names = TRUE)
+  envStack <- raster::stack(envFileSet)
 
-  # Which variables where used in the model fit?
-  featureBetas <- thisModel$betas
+  varImp <- fitMaxnet::varImportance(thisModel, occSWD, bkgSWD, "cloglog")
 
-  # Trim to non-zero coefficient values
-  if (any(featureBetas == 0))
-    featureBetas <- featureBetas[-which(featureBetas == 0)]
+  goodVars <- names(varImp)[varImp > varImpThresold]
 
-  tmpNames <- gsub("I(", "", names(featureBetas), fixed = TRUE)
-  ii <- grep("^2)", tmpNames, fixed = TRUE)
-  tmpNames[ii] <- gsub("^2)", "", tmpNames[ii], fixed = TRUE)
-  tmpNames[ii] <- paste0(tmpNames[ii],":", tmpNames[ii])
+  refMat <- as.matrix(rbind(occSWD[, 4:ncol(occSWD)],
+                            bkgSWD[, 4:ncol(bkgSWD)]))
+  refMat <- refMat[, goodVars]
 
-  varNames <- sort(unique(unlist(strsplit(tmpNames, ":", fixed = TRUE))))
+  varMin <- Rfast::colMins(refMat[, goodVars], value = TRUE)
+  names(varMin) <- goodVars
 
-  # Load as a raster stack only those variables in the model
-  #envDataFiles <- list.files(envDataPath, pattern = "tif", full.names = TRUE)
+  varMax <- Rfast::colMaxs(refMat[, goodVars], value = TRUE)
+  names(varMax) <- goodVars
 
-  #keepInd <- unlist(lapply(varNames, function(el) { grep(el, basename(envDataFiles)) }))
-  keepInd <- which(varNames %in% names(envData))
+  varRange <- varMax - varMin
+  names(varRange) <- goodVars
 
-  #envData <- raster::stack(envDataFiles[keepInd])
+  rasTemplate <- envStack[[1]]
 
-  envData <- envData[[keepInd]]
+  envMat <- raster::as.matrix(envStack)
+  #print(colnames(envMat))
+  envMat <- envMat[, goodVars]
 
-  goodCells <- which(!is.na(envData[[1]][]))
-  nGoodCells <- length(goodCells)
+  #remove(envStack)
+  rowStatus <- Rfast::rowAll(!is.na(envMat))
 
-  for (thisVar in varNames)
+  goodRows <- which(rowStatus)
+
+  N <- length(goodRows)
+  nRefVars <- length(goodVars)
+  nRefPts <- nrow(refMat)
+
+  varMESS <- matrix(0, N, nRefVars)
+  colnames(varMESS) <- goodVars
+
+  f <- matrix(0, N, nRefVars)
+  colnames(f) <- goodVars
+
+  p <- envMat[goodRows, goodVars]
+  #colnames(p) <- goodVars
+
+  # Borrow a clever idea from ecospat MESS implementation:
+  varECDF <- vector("list", nRefVars)
+  names(varECDF) <- goodVars
+
+  for (thisVar in goodVars)
   {
-varMin <- thisModel$varmin[thisVar]
-varMax <- thisModel$varmax[thisVar]
-#p_i <-
+    varECDF[[thisVar]] <- ecdf(refMat[, thisVar])
   }
 
-
-
-  # Load SWD file and do variable check
-  #refData <- read.csv(swdFilename, stringsAsFactors = FALSE)[, -c(1:3)]
-  refData <- swdData
-
-  if (!all(varNames %in% colnames(refData)))
-    stop("Variable names in model do not match variables names in the SWD file")
-  else
+  for (thisVar in goodVars)
   {
-    refData <- refData[, varNames]
+    f[, thisVar] <- 100*varECDF[[thisVar]](p[, thisVar])
   }
 
-  ans <- dismo::mess(x = envData, v = refData, full = MESSonly)
+  for (thisVar in goodVars)
+  {
+    firstInd <- na.omit(which(f[, thisVar] == 0))
+    if (length(firstInd) > 0)
+      varMESS[firstInd, thisVar] <- 100*(p[firstInd, thisVar] - varMin[thisVar])/varRange[thisVar]
+
+    secondInd <- which((f[, thisVar] > 0) & (f[, thisVar] <= 50))
+    if (length(secondInd) > 0)
+      varMESS[secondInd, thisVar] <- 2 * f[secondInd, thisVar]
+
+    thirdInd <- which((f[, thisVar] > 50) & (f[, thisVar] < 100))
+    if (length(thirdInd) > 0)
+      varMESS[thirdInd, thisVar] <- 2 * (100 - f[thirdInd, thisVar])
+
+    fourthInd <- which(f[, thisVar] == 100)
+    if (length(fourthInd) > 0)
+      varMESS[fourthInd, thisVar] <- 100*(varMax[thisVar] - p[fourthInd, thisVar])/varRange[thisVar]
+  }
+
+  MESS <- Rfast::rowMins(varMESS, value = TRUE)
+
+  rasTemplate[goodRows] <- MESS
+  plot(rasTemplate, main = "MESS")
+
+  if (outPath != "")
+  {
+    if (!dir.exists(outPath)) dir.create(outPath, recursive = TRUE)
+    outFilename <- file.path(outPath, "MESS.tif")
+    raster::writeRaster(rasTemplate, overwrite = TRUE, options = "COMPRESS=DEFLATE")
+  }
+
+  return(MESS)
 }
